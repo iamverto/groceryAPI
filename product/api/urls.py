3990ffc6a5baf720eab649dc93846b548fa2afe4
iamverto <nix0145@gmail.com>
2020-07-01 02:05:47 +0530

    admin api > product is working fine.

--- product/api/urls.py
+++ product/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ProductListCreateAPIView, ProductRetrieveUpdateDeleteAPIView
+from .views import ProductListCreateAPIView, ProductRetrieveUpdateDeleteAPIView, add_image, delete_image
 
 urlpatterns = [
     path('products/', ProductListCreateAPIView.as_view()),
     path('products/<int:id>/', ProductRetrieveUpdateDeleteAPIView.as_view()),
+    path('products/images/', add_image),
+    path('products/images/<int:id>/delete/', delete_image)
 ]
--- product/api/views.py
+++ product/api/views.py
@@ -1,12 +1,17 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
-from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
-from rest_framework.permissions import AllowAny
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, \
+    DestroyModelMixin
+from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
 from product.models import Product, ProductImage
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, ProductImageSerializer
 from rest_framework import filters
+from django.shortcuts import get_object_or_404
 
 
-class ProductListCreateAPIView(ListAPIView, ListModelMixin):
+class ProductListCreateAPIView(ListCreateAPIView, ListModelMixin, CreateModelMixin):
     serializer_class = ProductSerializer
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
     search_fields = ['title', 'description']
@@ -31,11 +36,53 @@ class ProductListCreateAPIView(ListAPIView, ListModelMixin):
     def get(self, request, *args, **kwargs):
         return self.list(request)
 
+    # only for admin
+    def post(self, request, *args, **kwargs):
+        if self.request.user.is_superuser:
+            print(request.data)
+            return self.create(request)
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
 
-class ProductRetrieveUpdateDeleteAPIView(RetrieveAPIView, RetrieveModelMixin):
+
+class ProductRetrieveUpdateDeleteAPIView(RetrieveUpdateDestroyAPIView, RetrieveModelMixin, UpdateModelMixin,
+                                         DestroyModelMixin):
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
     lookup_field = 'id'
 
     def get(self, request, *args, **kwargs):
         return self.retrieve(request)
+
+    def put(self, request, *args, **kwargs):
+        pass
+
+    # only for admin
+    def patch(self, request, *args, **kwargs):
+        if self.request.user.is_superuser:
+            return self.partial_update(request)
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+    # only for admin
+    def delete(self, request, *args, **kwargs):
+        if self.request.user.is_superuser:
+            return self.destroy(self, request)
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
+def add_image(request):
+    # data : product, pic
+    serializer = ProductImageSerializer(data=request.data, context={'request': request})
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    # print(serializer.data)
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['DELETE'])
+@permission_classes([IsAdminUser])
+def delete_image(request, id):
+    instance = get_object_or_404(ProductImage, id=id)
+    instance.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
